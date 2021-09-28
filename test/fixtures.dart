3c1950adbaf88dde04067506aa2cf074a76b303c
Robert Kaluzny <robertkaluzny33@gmail.com>
2021-09-28 17:04:05 +0200

    I added PhotoElementDTO and his test

--- test/fixtures.dart
+++ test/fixtures.dart
@@ -1,9 +1,23 @@
 import 'package:space_pictures/domain/failure/failure.dart';
 import 'package:space_pictures/domain/photo_list/photo_element.dart';
+import 'package:space_pictures/infrastructure/database_and_api/photo_element_dto.dart';
 
 Failure mockFailure = const Failure(failureId: 'failureId', failureValue: 'failureValue', failure: false);
 
 PhotoElement mockPhotoElement = PhotoElement(
+    failure: mockFailure,
+    apodSite: 'apodSite',
+    copyright: 'copyright',
+    date: 'date',
+    description: 'description',
+    hdurl: 'hdurl',
+    mediaType: 'mediaType',
+    title: 'title',
+    url: 'url',
+    localPath: 'localPath');
+
+
+PhotoElementDTO mockPhotoElementDto = PhotoElementDTO(
     failure: mockFailure,
     apodSite: 'apodSite',
     copyright: 'copyright',
---
+++ test/photo_element_dto/photo_element_dto_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:space_pictures/domain/photo_list/photo_element.dart';
+import 'package:space_pictures/infrastructure/database_and_api/photo_element_dto.dart';
+
+import '../fixtures.dart';
+
+
+
+void main() {
+  group('PhotoElementDto', () {
+    test('support value comparisons', () {
+      expect(mockPhotoElementDto, mockPhotoElementDto);
+    });
+
+    test('copyWith comparisons', () {
+      PhotoElementDTO mockPhotoElementDtoModified =  PhotoElementDTO(
+          failure: mockFailure,
+          apodSite: 'apodSite',
+          copyright: 'copyrightModified',
+          date: 'date',
+          description: 'description',
+          hdurl: 'hdurl',
+          mediaType: 'mediaType',
+          title: 'title',
+          url: 'url',
+          localPath: 'localPath');
+
+      expect(mockPhotoElementDto.copyWith(), mockPhotoElementDto);
+      expect(mockPhotoElementDto.copyWith(copyright: 'copyrightModified'), mockPhotoElementDtoModified);
+    });
+  });
+}
