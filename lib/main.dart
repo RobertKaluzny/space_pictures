abd20f3eb47222926f69e640baf3c69d0dc1ccd2
Robert Kaluzny <robertkaluzny33@gmail.com>
2021-09-30 22:39:53 +0200

    I added a vertical screen orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:space_pictures/injection.dart';
@@ -15,6 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       title: 'Space pictures',
       debugShowCheckedModeBanner: false,
